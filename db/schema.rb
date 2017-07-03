737e4b46378382027f8cf26344cd359b6b6cb277
attilaszek <attilaszek@gmail.com>
2017-07-03 17:54:17 +0300

    Add categories and answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703082252) do
+ActiveRecord::Schema.define(version: 20170703140650) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "name"
+    t.boolean "correct"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
